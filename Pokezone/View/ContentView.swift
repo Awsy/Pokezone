d52c0990afbd80dd1beaf924c4bb3d453a3512fe
Aws Shkara <bass_master89@mail.ru>
2025-08-15 13:41:56 +0100

    Added content view for unavailable data.

--- Pokezone/View/ContentView.swift
+++ Pokezone/View/ContentView.swift
@@ -11,63 +11,131 @@ import CoreData
 struct ContentView: View {
 	@Environment(\.managedObjectContext) private var viewContext
 	
-	@FetchRequest(
-		sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
-		animation: .default)
+	@FetchRequest<Pokemon>(sortDescriptors: [SortDescriptor(\.id)],
+						   animation: .default
+	) private var pokemons
 	
-	private var pokemons: FetchedResults<Pokemon>
-	let fetchPokemons = FetchingService()
+	private let fetchPokemons = FetchingService()
+	
+	
+	@State var searchText = ""
+	@State var isFavorite = false
+	
+	private var predicate: NSPredicate {
+		var predicates: [NSPredicate] = []
+		if !searchText.isEmpty {
+			predicates.append(NSPredicate(format: "name contains[c] %@", searchText))
+		}
+		
+		if isFavorite {
+			predicates.append(NSPredicate(format: "favorite == %d", true))
+		}
+		
+		return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+	}
 	
 	var body: some View {
-		NavigationStack {
-			List {
-				ForEach(pokemons) { pokemon in
-					NavigationLink(value: pokemon) {
+		
+		if pokemons.isEmpty {
+			ContentUnavailableView {
+				Label("No Pokemon", image: .nopokemon)
+			} description: {
+				Text("There is no pokemons list yet. \nFetch some pokemons to display.")
+			} actions: {
+				Button("Fetch Pokemons", systemImage: "antenna.radiowaves.left.and.right") {
+					getPokemons()
+				}
+				.buttonStyle(.borderedProminent)
+			}
+
+		} else {
+			
+			NavigationStack {
+				List {
+					Section {
+						ForEach(pokemons) { pokemon in
+							NavigationLink(value: pokemon) {
+								AsyncImage(url: pokemon.sprite) { image in
+									image
+										.resizable()
+										.scaledToFit()
+									
+								} placeholder: {
+									ProgressView()
+								}
+								.frame(width: 100, height: 100)
+								
+								VStack(alignment: .leading) {
+									
+									HStack {
+										Text(pokemon.name?.capitalized ?? "")
+											.fontWeight(.bold)
+										
+										if pokemon.favorite {
+											Image(systemName: "star.fill")
+												.foregroundStyle(.yellow)
+												.padding()
+										}
+									}
+									
+									HStack {
+										ForEach(pokemon.types!, id: \.self) { type in
+											Text(type.capitalized)
+												.font(.subheadline)
+												.fontWeight(.semibold)
+												.foregroundStyle(.black)
+												.padding(.horizontal, 10)
+												.padding(.vertical, 5)
+												.background(Color(type.capitalized))
+												.clipShape(.capsule)
+										}
+									}
+								}
+								
+							}
+						}
+					}
+					
+				}
+				.navigationTitle("Pokezone")
+				.searchable(text: $searchText, prompt: "Search a Pokemon...")
+				.autocorrectionDisabled()
+				.onChange(of: searchText) {
+					pokemons.nsPredicate = predicate
+				}
+				.onChange(of: isFavorite) {
+					pokemons.nsPredicate = predicate
+				}
+				.navigationDestination(for: Pokemon.self) { pokemon in
+					HStack {
 						AsyncImage(url: pokemon.sprite) { image in
-								image
+							image
 								.resizable()
 								.scaledToFit()
-								
+							
 						} placeholder: {
 							ProgressView()
 						}
 						.frame(width: 100, height: 100)
 						
-						VStack(alignment: .leading) {
-							Text(pokemon.name ?? "")
+						Text(pokemon.name ?? "No Name found")
+					}
+				}
+				.toolbar {
+					ToolbarItem(placement: .navigationBarTrailing) {
+						Button {
+							isFavorite.toggle()
+						} label: {
+							Label("Filter Pokemons", systemImage: isFavorite ? "star.fill" : "star")
 						}
-						
+						.tint(.yellow)
 					}
 				}
 				
 			}
-			.navigationTitle("Pokezone")
-			.navigationDestination(for: Pokemon.self) { pokemon in
-				HStack {
-					AsyncImage(url: pokemon.sprite) { image in
-						image
-							.resizable()
-							.scaledToFit()
-						
-					} placeholder: {
-						ProgressView()
-					}
-					.frame(width: 100, height: 100)
-					
-					Text(pokemon.name ?? "No Name found")
-				}
+			.task {
+				getPokemons()
 			}
-			.toolbar {
-				ToolbarItem(placement: .navigationBarTrailing) {
-					EditButton()
-				}
-				ToolbarItem {
-					Button("Add Item", systemImage: "plus") {
-						getPokemons()
-					}
-				}
-			}
-			
 		}
 	}
 	
@@ -89,6 +157,10 @@ struct ContentView: View {
 					pokemon.speed = fetchedPokemons.speed
 					pokemon.shiny = fetchedPokemons.shiny
 					pokemon.sprite = fetchedPokemons.sprite
+					
+					if pokemon.id % 2 == 0 {
+						pokemon.favorite = true
+					}
 					
 					try viewContext.save()
 					
